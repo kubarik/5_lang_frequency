b2ff65063f80630da90f4665c08468fc9d9e1113
kubarik <webpavlova@gmail.com>
2018-01-18 07:18:07 +0300

    review

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,33 +1,19 @@
-import io
 import collections
-import operator
 import re
 import sys
 
 
-total_words_count = {}
-
-
 def load_data(file_path):
     try:
-        with io.open(file_path, encoding='utf-8') as file_handler:
-            for line_text in file_handler:
-                words_count = get_words_count(line_text)
-                save_count_word(words_count)
+        with open(file_path, encoding='utf8') as file_handler:
+            return file_handler.read()
     except OSError:
         return False
 
 
-def get_words_count(text):
+def get_most_frequent_words(text):
     words = re.findall(r'\w+', text.lower().strip())
-    return collections.Counter(words).items()
-
-
-def save_count_word(words_count):
-    global total_words_count
-    for word in words_count:
-        total = total_words_count.get(word[0])
-        total_words_count[word[0]] = total+word[1] if total is not None else word[1]
+    return collections.Counter(words).most_common(10)
 
 
 if __name__ == '__main__':
@@ -35,8 +21,8 @@ if __name__ == '__main__':
         sys.exit('Не указан файл')
 
     input_file_path = sys.argv[1]
-    load_data(input_file_path)
-    if len(total_words_count):
-        total_words_count_sorted = sorted(total_words_count.items(), key=operator.itemgetter(1), reverse=True)
-        for word, count in total_words_count_sorted[:10]:
-            print(word, count)
+    load_text = load_data(input_file_path)
+    frequent_words = get_most_frequent_words(load_text)
+    for word, count in frequent_words:
+        print(word, count)
+
